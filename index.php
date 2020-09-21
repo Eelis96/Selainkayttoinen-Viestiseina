3f1c4a70053c84cee4d091733c807c9764c94c85
Eelis96 <57720310+Eelis96@users.noreply.github.com>
2020-09-21 08:46:01 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Viestiseinä</title>
+    <link rel="stylesheet" href="bootstrap.min.css">
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <?php
+            if(!isset($_SESSION['logged_in'])){
+                echo '<a class="btn btn-outline-secondary" href="loginform.php">Kirjaudu Sisään</a>';
+            }else{
+                echo'<a class="btn btn-outline-danger" href="logout.php">Kirjaudu Ulos</a>';
+            }
+        ?>
+    </nav>
+
+    <div class="jumbotron">
+        <h1>Viestiseinä</h1>
+    </div>
+
+</body>
+</html>
